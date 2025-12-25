d64bed44b685b114cfc4cfa95b7d4de50d19970b
Rehan <riyanzakir2003@gmail.com>
2025-12-26 04:05:32 +0500

    Update news.js

--- api/news.js
+++ api/news.js
@@ -1,1 +1,17 @@
+export default async function handler(req, res) {
+  const category = req.query.category || "general";
+
+  const url =
+    `https://gnews.io/api/v4/top-headlines?country=pk&category=${category}&apikey=${process.env.GNEWS_KEY}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch news" });
+  }
+}
 
